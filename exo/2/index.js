0df2e5897861c599f29dab849d99aac915b24660
Neoned is Here 〆 <neonedishere@gmail.com>
2025-09-17 14:39:16 +0200

    .

--- exo/2/index.js
+++
@@ -1,6 +0,0 @@
-const name = "Jeff"
-const age = 28
-const text = " Bonjour " + name + " tu as " + age + " ans aujourd’hui, c’est la fiesta !"
-
-console.log(text)
-console.log(text.length)
--- index.js
+++
@@ -1,9 +0,0 @@
-const monTexte = document.querySelector('textarea')
-const maDiv = document.querySelector('div')
-
-monTexte.addEventListener('keyup', (e) => {
-
-    console.log(e)
-    maDiv.innerHTML = marked(monTexte.value )
-    localStorage.setItem(maDiv, monTexte.value)
-})
